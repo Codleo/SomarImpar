d21f8f9e57d590061f57adfe8d6b5dfbd9e0d3ca
Leonardo dos Santos <leo.santos2001@hotmail.com>
2024-06-20 15:29:35 -0300

    Adicionar arquivos de projeto.

---
+++ SomarImpar/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int soma = 0;
+
+        for (int numero = 100; numero <= 200; numero++)
+        {
+            // Verifica se o número é ímpar
+            if (numero % 2 != 0)
+            {
+                // Se for ímpar, adiciona à soma
+                soma += numero;
+            }
+        }
+        Console.WriteLine($"A soma dos números ímpares entre 100 e 200 é: {soma}");
+    }
+}
